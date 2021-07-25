e5cbc3f7de4dbea9bc328d6d7bcb57145afeb61d
Artem-Rudko <72283896+Artem-Rudko@users.noreply.github.com>
2021-07-25 23:43:57 +0300

    add navigation component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
+import HomeView from './views/HomeView';
+import MoviesView from './views/MoviesView';
 // import axios from 'axios';
 // import Loader from 'react-loader-spinner';
 // import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
@@ -8,13 +11,36 @@ import './styles.css';
 // const BASE_URL = 'https://pixabay.com/api/';
 // const LOG = '21433732-4f4ab4e06b98cffafd914747a';
 
-class App extends Component {
-    state = {};
-
-    render() {
-        return <div className="App"></div>;
-    }
-}
+const App = () => (
+    <>
+        <ul>
+            <li>
+                <NavLink
+                    exact
+                    to="/"
+                    className="NavLink"
+                    activeClassName="NavLink--active"
+                >
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to="/movies"
+                    className="NavLink"
+                    activeClassName="NavLink--active"
+                >
+                    Movies
+                </NavLink>
+            </li>
+        </ul>
+        <Switch>
+            <Route exact path="/" component={HomeView} />
+            <Route path="/movies" component={MoviesView} />
+            <Route component={HomeView} />
+        </Switch>
+    </>
+);
 
 // App.defaultProps = {};
 
