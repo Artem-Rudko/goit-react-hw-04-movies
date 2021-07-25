a688dc6213b6d765f3f44e6b074f8dd794e0d0e0
Artem-Rudko <72283896+Artem-Rudko@users.noreply.github.com>
2021-07-25 22:40:32 +0300

    add initial files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+// import axios from 'axios';
+// import Loader from 'react-loader-spinner';
+// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+// import PropTypes from 'prop-types';
+import './styles.css';
+
+// const BASE_URL = 'https://pixabay.com/api/';
+// const LOG = '21433732-4f4ab4e06b98cffafd914747a';
+
+class App extends Component {
+    state = {};
+
+    render() {
+        return <div className="App"></div>;
+    }
+}
+
+// App.defaultProps = {};
+
+// App.propTypes = {};
+
+export default App;
