4c92b5ee3a34f889db5b8bfb35bbae93d9a58389
Artem-Rudko <72283896+Artem-Rudko@users.noreply.github.com>
2021-08-06 23:33:12 +0300

    add chunk*s name

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import routes from './routes';
 import AppBar from './components/AppBar/AppBar';
 import './styles.css';
 
-const HomeView = lazy(() => import('./views/HomeView.js'));
-const MovieDetailsView = lazy(() => import('./views/MovieDetailsView.js'));
-const MoviesView = lazy(() => import('./views/MoviesView.js'));
+const HomeView = lazy(() =>
+    import('./views/HomeView.js' /*webpackChunkName: "home-view" */),
+);
+const MovieDetailsView = lazy(() =>
+    import(
+        './views/MovieDetailsView.js' /*webpackChunkName: "movie-details-view" */
+    ),
+);
+const MoviesView = lazy(() =>
+    import('./views/MoviesView.js' /*webpackChunkName: "movies-view" */),
+);
 
 const App = () => (
     <>
